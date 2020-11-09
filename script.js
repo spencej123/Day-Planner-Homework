215e2b80a783bb68bb468b04ccaaf00a06051ed1
Spencer Johnson <spencer.johnson123@hotmail.com>
2020-11-08 23:12:47 -0600

    trying to getting moment.js set up

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //Starting With Moment.js
-
+let currentDate =
+  moment().format("dddd") + " " + moment().format("Do MMM YYYY");
+let currentHour = moment().format("h:mm:ss a");
 //Making the Time/hour Variables
 
 //Date/Time/String
